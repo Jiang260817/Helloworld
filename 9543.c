ce990b79a42f6747a8be34c7ec399b4288362abe
Mr.Toad <56026274+Jiang260817@users.noreply.github.com>
2021-10-27 22:24:05 +0800

    Create 9543.c
    Programming 9543

---
+++ 9543.c
@@ -0,0 +1,14 @@
+//9543
+#include "stdio.h"
+int  main()
+{
+float x,y,z1,z2,z3,z4; 
+
+scanf("%f,%f",&x,&y);
+z1=x+y;
+z2=x-y;
+z3=x*y;
+z4=x/y;
+printf("x+y=%.2f\nx-y=%.2f\n",z1,z2); 
+printf("x*y=%.2f\nx/y=%.2f\n",z3,z4); 
+}
